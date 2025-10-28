0e71e6f4e35b415e7d88516e4fca9cb29627668f
Erick Sanchez <sanchezjoserick@gmail.com>
2025-10-28 06:17:08 -0600

    [ :sparkles: ] feat: rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,24 @@ import { MoviesModule } from './movies/movies.module';
 import { AuthModule } from './auth/auth.module';
 import { envs } from './config';
 import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [UsersModule, MoviesModule, AuthModule, JwtModule.register({
     global: true, secret: envs.JWT_SECRET,
-  })],
+  }), ThrottlerModule.forRoot({
+    throttlers: [
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ],
+  }),],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
 export class AppModule {}
